3b19e61bd9270304bf85693adc37aef241faa8c6
Unknown <beauquel.alexandre@gmail.com>
2019-03-20 00:16:31 -0400

    Driver et base de données pour cache locale sqlite #2

---
+++ Clients/ClientVanille/data/inspiration.cache.sql
@@ -0,0 +1,27 @@
+
+PRAGMA foreign_keys = 0;
+
+CREATE TABLE sqlitestudio_temp_table AS SELECT *
+                                          FROM pensee;
+
+DROP TABLE pensee;
+
+CREATE TABLE pensee (
+    id      INTEGER      PRIMARY KEY AUTOINCREMENT,
+    auteur  STRING (255),
+    message TEXT
+);
+
+INSERT INTO pensee (
+                       id,
+                       auteur,
+                       message
+                   )
+                   SELECT id,
+                          auteur,
+                          message
+                     FROM sqlitestudio_temp_table;
+
+DROP TABLE sqlitestudio_temp_table;
+
+PRAGMA foreign_keys = 1;
