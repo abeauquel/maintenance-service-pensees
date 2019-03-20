ca27466be2a6b7594d4ffec2762f78f140a5075a
Unknown <beauquel.alexandre@gmail.com>
2019-03-20 00:12:18 -0400

    Script non-optimisé pour obtenir la pensée du jour - qui sera cachée par le client pour #2

--- Service/src/pensee/liste/index.php
+++ Service/src/pensee/liste/index.php
@@ -1,30 +1,19 @@
-<?php 
+<?php
+include "../../accesseur/PenseeDAO.php";
+$penseeDAO = new PenseeDAO();
+//print_r($penseeDAO);
+$listePensees = $penseeDAO->listerPensees();
+//print_r($listePensees);
+$indexAleatoire = rand(0, count($listePensees) - 1);
+$pensee = $listePensees[$indexAleatoire];
 
-	include "../../accesseur/PenseeDAO.php";
-	$penseeDAO = new PenseeDAO();
-	//print_r($penseeDAO);
-	$listePensees = $penseeDAO->listerPensees();
-	//print_r($listePensees);
-?><?php 
-
+?><?php
 header("Content-type: text/xml");
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
-<pensees>
-	<nombre><?=count($listePensees)?></nombre>
-<?php
-//utf8_encode()
-foreach($listePensees as $pensee)
-{
-	//print_r($pensee);
-	?>
-	<pensee>
-		<auteur><?=($pensee->auteur)?></auteur>
-		<message><?=($pensee->message)?></message>
-		<annee><?=$pensee->annee?></annee>
-		<id><?=$pensee->idPensee?></id>
-	</pensee>
-	<?php
-}
-?>
-</pensees>
+<pensee>
+    <auteur><?=($pensee->auteur)?></auteur>
+    <message><?=($pensee->message)?></message>
+    <annee><?=$pensee->annee?></annee>
+    <id><?=$pensee->idPensee?></id>
+</pensee>
