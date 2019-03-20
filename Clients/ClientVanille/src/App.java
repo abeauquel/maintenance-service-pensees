197081df80472797dc84578716536c74f1bf2e93
Unknown <beauquel.alexandre@gmail.com>
2019-03-20 00:18:00 -0400

    Test SQL liste avec liste

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,4 +1,8 @@
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,6 +14,23 @@ import vue.VueInspirationVisuelle;
 public class App {
 
 	public static void main(String[] parametres) {
+
+		String DSN = "jdbc:sqlite:C:/sqlite/bases/inspiration.cache.db";
+		String SQL_ENREGISTRER_PENSEE = "insert into pensee(auteur, message) values('','')";
+		String SQL_LISTER_PENSEES = "SELECT * FROM pensee";
+
+		Connection basededonnees = null;
+		try {
+			basededonnees = DriverManager.getConnection(DSN);
+			Statement requeteListePensees = basededonnees.createStatement();
+			ResultSet curseurListePensees = requeteListePensees.executeQuery(SQL_LISTER_PENSEES);
+			curseurListePensees.next();
+			String auteur = curseurListePensees.getString("auteur");
+			String pensee = curseurListePensees.getString("message");
+			System.out.println(auteur + " - " + pensee);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 		//Journal.activer();
 		Journal.activerNiveau(0);
